6f33f102335c9364eaef8478cbc1bcbba8532506
stefano-terna <71135699+stefano-terna@users.noreply.github.com>
2024-02-06 16:04:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import yfinance as yf
 import pandas as pd
+import plotly.graph_objs as go
 
 st.title('Visualizzatore di Dati di Mercato')
 
@@ -8,11 +9,37 @@ st.title('Visualizzatore di Dati di Mercato')
 symbol = st.text_input('Inserisci il simbolo dell\'azione (es. TRN.MI):', 'TRN.MI')
 
 # Utilizzo di yfinance per ottenere i dati
-data = yf.Ticker(symbol)
+ticker_data = yf.Ticker(symbol)
 
 # Visualizzazione di alcuni dati di base
-st.write(data.info['longBusinessSummary'])
+st.write(ticker_data.info['longBusinessSummary'])
 
-# Ottenere e visualizzare i dati storici
-hist_data = data.history(period="10y")  # dati dell'ultimo mese
-st.line_chart(hist_data['Close'])
+data = yf.download(symbol, period='10y')
+data.reset_index(inplace=True)
+
+# Funzione per creare il grafico
+def create_chart(x_range=None):
+    fig = go.Figure(data=[go.Candlestick(x=data['Date'],
+                    open=data['Open'], high=data['High'],
+                    low=data['Low'], close=data['Close'])])
+
+    fig.update_layout(xaxis_rangeslider_visible=True)
+
+    if x_range:
+        fig.update_xaxes(range=x_range)
+
+    return fig
+
+# Pulsanti per il reset
+if st.button('Reset a Gennaio 2021'):
+    fig = create_chart(x_range=['2021-01-01', '2021-01-31'])
+    st.plotly_chart(fig)
+elif st.button('Mostra Tutto'):
+    fig = create_chart()  # Nessun x_range specificato, mostra tutto
+    st.plotly_chart(fig)
+else:
+    # Grafico di default al caricamento della pagina
+    fig = create_chart()
+    st.plotly_chart(fig)
+
+
